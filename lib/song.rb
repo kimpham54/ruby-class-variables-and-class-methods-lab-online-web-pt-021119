14be05d78057377494b21a28a79d3f4fc0ce1a51
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-03 04:40:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,35 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
-@@artists = 0
-@@genres = 0
+@@artists = []
+@@genres = []
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
+  @@count += 1
+  if @@artists.include?(artist) == false
+  @@artists.push(artist)
 end
+end
+
+def self.count
+  @@count
+end
+
+def self.artists
+
+end
+
+def self.genres
+
+end
+
+def self.genre_count
+
+end
+
 
 
 
